c256eda4add1ba5a40a0fc929f74a99b728c6881
shafia-dal <shafia.dal@operisoft.com>
2025-04-09 14:18:15 +0530

    remove space before account id pull_image.sh

--- scripts/pull_image.sh
+++ scripts/pull_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-AWS_ACCOUNT_ID= 091846656105
+AWS_ACCOUNT_ID=091846656105
 AWS_REGION=us-east-1
 IMAGE_REPO_NAME="my-nodejs-repo" # Ensure this matches your buildspec
 IMAGE_TAG=latest # Get the specific commit/build ID
