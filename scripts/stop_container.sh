a52e500cd2040d0cf716a0f2f9519211510ad7b9
Ubuntu <jayeshnaphade00@gmail.com>
2025-04-09 07:08:32 +0000

    permissions denied error resolved

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Replace 'my-nodejs-container' with the actual name of your Docker container
-docker stop my-nodejs-container
-docker rm my-nodejs-container
+docker stop my-nodejs-app
+docker rm my-nodejs-app
