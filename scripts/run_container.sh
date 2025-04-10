c53a21e6b7b60f60bbb7cce97be2e42d75a37d79
Ubuntu <jayeshnaphade00@gmail.com>
2025-04-10 09:26:15 +0000

    sab solve hogaya

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -10,7 +10,6 @@ echo "  Port Mapping: -p $APP_PORT:$APP_PORT"
 
 docker run -d --name my-nodejs-repo \
     -p 4000:4000 \
-    -e NODE_ENV=production \
     $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
 
     if [ $? -eq 0 ]; then
